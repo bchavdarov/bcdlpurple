3d83272d64ca2c061509b7840c09b8d37af9efe5
bchavdarov <spo@abv.bg>
2022-07-02 12:35:50 +0300

    updated files

--- inc/bcdl-headerclean.php
+++ inc/bcdl-headerclean.php
@@ -4,33 +4,37 @@
  * Adds posts and media for the header
 */
 
-//bcdl_section_add( $wp_customize, 200, 'bcdl-headerclean', 'Header section', 'Header settings' );
+bcdl_section_add( $wp_customize, 200, 'bcdl-header', 'Header section', 'Header settings' );
 
 
-$wp_customize->add_section( 'bcdl-header' , 
+/*$wp_customize->add_section( 'bcdl-header' , 
 		array(
 	    'title'      => __( 'Frontpage Header', 'bcdlpurple' ),
 	    'description' => __( 'Select image for the header.', 'bcdlpurple' ),
 	    'priority'   => 200,
 		) 
 	);
+*/
 
-	$wp_customize->add_setting( 'bcdl-header-set', 
-		array( 
-			'type' => 'theme_mod',
-			'default' => '',
-			'transport' => 'refresh',
-      'sanitize_callback' => 'absint'
-		) 
-	);
+$wp_customize->add_setting( 'bcdl-header-set', 
+  array( 
+    'type' => 'theme_mod',
+    'default' => '',
+    'transport' => 'refresh',
+    'sanitize_callback' => 'absint'
+  ) 
+);
 
-	$wp_customize->add_control( new WP_Customize_Media_Control( 
-		$wp_customize, 'bcdl-headerctrl', 
-			array( 
-				'label' => __( 'Select Header image', 'bcdlpurple' ), //check the .pot file
-				'section' => 'bcdl-header', 
-				'settings' => 'bcdl-header-set',
-			) 
-		) 
-	);
-?>
+$wp_customize->add_control( new WP_Customize_Media_Control( 
+  $wp_customize, 'bcdl-headerctrl', 
+    array( 
+      'label' => __( 'Select Header image', 'bcdlpurple' ), //check the .pot file
+      'section' => 'bcdl-header', 
+      'settings' => 'bcdl-header-set',
+    ) 
+  ) 
+);
+
+bcdl_section_titles_add( $wp_customize, 'bcdl-header', 'bcdl_headerttl', 'bcdl_headerttl-ctrl', 'text', 'Section title' );
+
+bcdl_section_titles_add( $wp_customize, 'bcdl-header', 'bcdl_headerstl', 'bcdl_headerstl-ctrl', 'textarea', 'Section subtitle' );
--- template-parts/section-headerclean.php
+++ template-parts/section-headerclean.php
@@ -8,6 +8,8 @@
  */
 
 $headerId = get_theme_mod( 'bcdl-header-set' ); 
+$headerTtl = get_theme_mod( 'bcdl_headerttl' );
+$headerStl = get_theme_mod( 'bcdl_headerstl' );
 
 ?>
 
@@ -16,9 +18,9 @@ $headerId = get_theme_mod( 'bcdl-header-set' );
 	<div class="container h-100">
 		<div class="row h-100 align-items-center justify-content-end">
 			<div class="col-md-6 text-white" style="background-color:rgba(9,37,93,0.09);">
-				<h2 class="pt-4 display-3 bcdl-osc">Еилфикс България ООД</h2>
+				<h2 class="pt-4 display-3 bcdl-osc"><?php echo $headerTtl ?></h2>
 				<hr class="">
-				<p class="pb-4 display-6 bcdl-os">Винаги едно чисто решение</p>
+				<p class="pb-4 display-6 bcdl-os"><?php echo $headerStl ?></p>
 			</div>
 		</div>
 	</div>
